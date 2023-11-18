ca04021fd208d37620eb7ecaaa32c50bfaed4897
wakare <wakare_no_kaze@outlook.com>
2023-11-18 21:54:14 +0800

    [Feature] Fix instance transform bug

--- glTFRenderer/glTFRenderer/glTFRenderPass/glTFRenderMeshManager.cpp
+++ glTFRenderer/glTFRenderer/glTFRenderPass/glTFRenderMeshManager.cpp
@@ -34,7 +34,7 @@ bool glTFRenderMeshManager::AddOrUpdatePrimitive(glTFRenderResourceManager& reso
     }
 
     // Only update when transform has changed
-    m_mesh_instances[instance_ID].m_instance_transform = primitive->GetTransformMatrix();
+    m_mesh_instances[instance_ID].m_instance_transform = transpose(primitive->GetTransformMatrix());
     m_mesh_instances[instance_ID].m_instance_material_id = primitive->HasMaterial() ? primitive->GetMaterial().GetID() : glTFUniqueIDInvalid;
     m_mesh_instances[instance_ID].m_normal_mapping = primitive->HasNormalMapping();
     m_mesh_instances[instance_ID].m_mesh_render_resource = mesh_ID;
