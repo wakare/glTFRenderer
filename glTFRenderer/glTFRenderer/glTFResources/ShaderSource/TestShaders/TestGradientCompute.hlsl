2aec7526b033b57581357587c91833334602aca4
wakare <wakare_no_kaze@outlook.com>
2024-06-27 17:42:45 +0800

    [Feature] Add push constant for HelloVulkan compute shader test

--- glTFRenderer/glTFRenderer/glTFResources/ShaderSource/TestShaders/TestGradientCompute.hlsl
+++ glTFRenderer/glTFRenderer/glTFResources/ShaderSource/TestShaders/TestGradientCompute.hlsl
@@ -1,7 +1,6 @@
 RWTexture2D<float4> Output: register(u0, space0);
 
-[[vk::push_constant]]
-cbuffer ConstantBufferData
+struct ConstantBufferData
 {
     float4 data0;
     float4 data1;
@@ -9,8 +8,11 @@ cbuffer ConstantBufferData
     float4 data3;
 };
 
+[[vk::push_constant]]
+ConstantBuffer<ConstantBufferData> constant_buffer;
+
 [numthreads(8, 8, 1)]
 void main(int3 dispatchThreadID : SV_DispatchThreadID)
 {
-    Output[dispatchThreadID.xy] = float4(data0.xyz, 1.0);
+    Output[dispatchThreadID.xy] = float4(constant_buffer.data0.xyz, 1.0);
 }
